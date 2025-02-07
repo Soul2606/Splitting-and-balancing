8576be177b5aeb5a6c2741e849b8379d2cdab80e
Soul2606 <sondulv@gmail.com>
2025-02-07 22:10:32 +0100

    DLVT is completed (:

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ class Balancer{
 	constructor(out_a,out_b, name){
 		this.name = name // For debug purposes
 		this.oscillation = false // flips between true and false for every balance
-		this.in_a = new Single_item_container(null, this)
-		this.in_b = new Single_item_container(null, this)
+		this.in_a = new Doubly_linked_variable_transporter(null, this)
+		this.in_b = new Doubly_linked_variable_transporter(null, this)
 		this.out_a = out_a // is an pointer
 		this.out_b = out_b // is an pointer
 	}
 
 	transfer(input,output){
 
-		if (input.value === null || !input instanceof Single_item_container || !output instanceof Single_item_container) {
+		if (input.value === null || !input instanceof Doubly_linked_variable_transporter || !output instanceof Doubly_linked_variable_transporter) {
 			return false
 		}
 
@@ -60,15 +60,16 @@ class Balancer{
 
 
 
-const sic_pointers = new Set()
+const dlvt_pointers = new Set()
 
 
 
 
-class Single_item_container{
+class Doubly_linked_variable_transporter{
 	constructor(value){
 		this.value = value? value: null
 		this.transfer_target = null
+		this.pending_item_to_send = null
 		this.pending_send_to = null
 		this.pending_receive_from = null
 	}
@@ -77,25 +78,24 @@ class Single_item_container{
 		if (this.transfer_target === transfer_target){	
 			return true
 		}
-		if (sic_pointers.has(transfer_target)) {
-			console.warn('target already exists in sic_pointers')
+		if (dlvt_pointers.has(transfer_target)) {
+			console.warn('target already exists in dlvt_pointers')
 			return false
 		}
 		if (this.transfer_target !== null) {
-			console.log('removing from sic_pointers:', this.transfer_target)
-			sic_pointers.delete(this.transfer_target)
+			console.log('removing from dlvt_pointers:', this.transfer_target)
+			dlvt_pointers.delete(this.transfer_target)
 		}
 		this.transfer_target = transfer_target
-		sic_pointers.add(transfer_target)
+		dlvt_pointers.add(transfer_target)
 		return true
 	}
 
 	prepare_transfer(){
-		if(this.transfer_target.pending_receive_from !== null){
-			console.log('Hit self, loop successfully formed')
-			return true
+		if (this.transfer_target === undefined) {
+			console.warn(this.transfer_target, ', is undefined instead of null')
 		}
-		if (this.transfer_target === null) {
+		if (this.transfer_target === null || this.transfer_target === undefined) {
 			console.log('Hit end, path successfully formed')
 			// Transfer can only proceed if the final SIC is empty
 			if (this.value === null) {
@@ -104,10 +104,28 @@ class Single_item_container{
 				return false
 			}
 		}
-		console.log('recursion from:', this)
+		if(this.transfer_target.pending_receive_from !== null){
+			console.log('Hit self, loop successfully formed')
+			return true
+		}
+		this.pending_item_to_send = this.value
 		this.pending_send_to = this.transfer_target
 		this.transfer_target.pending_receive_from = this
-		return this.transfer_target.prepare_transfer()
+		const can_transfer = this.transfer_target.prepare_transfer()
+		if (can_transfer) {
+			console.log('transferring:', this.pending_item_to_send, 'to:', this.transfer_target.value)
+			// This must happen first
+			if (this.pending_receive_from === null) {
+				this.value = null
+			}
+			this.transfer_target.value = this.pending_item_to_send
+		}else{
+			console.log('transfer failed')
+		}
+		this.pending_item_to_send = null
+		this.pending_send_to = null
+		this.pending_receive_from = null
+		return can_transfer
 	}
 
 }
@@ -117,13 +135,15 @@ class Single_item_container{
 
 
 
-let sic_1 = new Single_item_container('A')
-let sic_2 = new Single_item_container('B')
+let dlvt_1 = new Doubly_linked_variable_transporter('A')
+let dlvt_2 = new Doubly_linked_variable_transporter('B')
+let dlvt_3 = new Doubly_linked_variable_transporter()
 
-sic_1.set_target(sic_2)
-sic_2.set_target(sic_1)
+dlvt_1.set_target(dlvt_2)
+dlvt_2.set_target(dlvt_3)
 
-console.log(sic_1)
-console.log(sic_2)
+console.log( dlvt_1, dlvt_2, dlvt_3)
 
-console.log(sic_1.prepare_transfer())
+console.log(dlvt_1.prepare_transfer())
+
+console.log( dlvt_1, dlvt_2, dlvt_3)
